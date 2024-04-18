09f3a6b6026503515f3780062cf41eb9ffec93f5
Toobasheikh21 <sheikhtooba140@gmail.com>
2024-04-19 01:25:50 +0500

    complete calculator cli based

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node 
+import inquirer from "inquirer";
+//Asking Question from Users through inquirer
+const answer = await inquirer.prompt([
+    { message: "Enter firstNumber", type: "number", name: "firstNumber" },
+    { message: "Enter secondNumber", type: "number", name: "secondNumber" },
+    {
+        message: "Select one Operator to perform opertion",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    },
+]);
+//Conditional statement If-Else 
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else {
+    console.log("Invalid Input");
+}
